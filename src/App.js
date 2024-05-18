c69e5b4f96607b6e51b71768b326735d047b1168
Muhammad Asad <asadghafoor26.figma@gmail.com>
2024-05-18 15:10:05 +0500

    route impleted & data is fetched from the csv sheet

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Navbar from './component/navbar/Navbar';
 import Product from './component/product/Product';
 import Order from './component/order/Order';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Order></Order>
-      {/* <Product></Product> */}
+      {/* <Navbar/>
+      <Product></Product> */}
+      {/* <Order></Order> */}
+      <BrowserRouter>
+      <Navbar/> 
+        <Routes>
+          <Route path="/product" element={<Product/>} />
+          <Route path="/" element={<Order/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/order/Order.jsx
+++ src/component/order/Order.jsx
@@ -1,11 +1,32 @@
+import { useNavigate } from "react-router-dom";
 import "./Order.css";
 function Order() {
+  const navigate = useNavigate();
+  async function getOrders(e) {
+    e.preventDefault();
+    const result = await fetch(
+      `https://sheetdb.io/api/v1/dvb7xxssna89k`,
+
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/json",
+        },
+      }
+    );
+    const response = await result.json();
+    // navigate('/about', { state: { from: 'Home Page', userId: 123 } });
+
+  }
   return (
     <>
-      <div className="container">
+      <div className="container order_container">
         <div className="row">
           <div className="program_control">
-            <button>Pause / Start Autoprogram</button>
+
+          {/* onClick={(e) => getOrders(e)} */}
+            <button type="button" >Pause / Start Autoprogram</button>
           </div>
         </div>
 
